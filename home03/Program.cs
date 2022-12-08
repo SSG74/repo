0e3b61cb81c47aa4e9f430dbad86208f9310adf4
Svutlana <fire_fly_74@mail.ru>
2022-12-08 23:30:38 +0500

    home03

---
+++ home03/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int resalt = number%2;
+
+if (resalt == 0)
+{
+    Console.WriteLine("Число четное");
+}
+
+else
+{
+    Console.WriteLine("Число нечетное");
+}
