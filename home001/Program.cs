4230a75ae34709be26f4b4879683e1a8ea124899
Svutlana <fire_fly_74@mail.ru>
2022-12-08 22:49:27 +0500

    home001

---
+++ home001/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine($"Число {a} является большим, число {b} является меньшим");
+}
+
+else
+{
+    Console.WriteLine($"Число {b} является большим, число {a} является меньшим");
+}
