568961b2c94362dad2b30a3a95d9d485dfe1f79c
Svutlana <fire_fly_74@mail.ru>
2022-12-08 22:35:53 +0500

    home002

---
+++ home002/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число: ");
+int c = int.Parse(Console.ReadLine());
+int max = a;
+
+if (b > a)
+max = b;
+if (c > b)
+max = c;
+Console.WriteLine(max);
