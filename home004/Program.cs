ac0db0c83c1af35e9ab348d9e6223f666086a59c
Svutlana <fire_fly_74@mail.ru>
2022-12-08 23:26:55 +0500

    home004

---
+++ home004/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int b = 2;
+
+
+while (b <= a)
+{
+    Console.WriteLine(b);
+    b+=2;
+}
